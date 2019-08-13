af596a15e2de7f5ceb3b7dea14ef6663036e43c8
Donya Moxley <moxleydevelopment@gmail.com>
2019-08-13 18:10:42 -0400

    added container components to client

---
+++ client/src/components/PaymentContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PaymentContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default PaymentContainer;
---
+++ client/src/components/ProductContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ProductContainer;
