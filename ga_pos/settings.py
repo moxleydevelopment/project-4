1150935cbe6db0ea0b428aa644188aeb92975617
Donya Moxley <moxleydevelopment@gmail.com>
2019-08-13 10:27:30 -0400

    changed settings added views.py

--- ga_pos/settings.py
+++ ga_pos/settings.py
@@ -15,6 +15,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+REACT_APP_DIR = os.path.join(BASE_DIR, 'client')
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,4 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(REACT_APP_DIR, 'build', 'static')
+]
 django_heroku.settings(locals())
