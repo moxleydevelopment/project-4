f39ad6baca279baefcef06e70340d22350129fa5
Donya Moxley <moxleydevelopment@gmail.com>
2019-08-13 13:45:03 -0400

    added admin functionality

--- pos_app/admin.py
+++ pos_app/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Transaction, User, Product, Ingredient
 
-# Register your models here.
+admin.site.register([User, Transaction, Product, Ingredient])
